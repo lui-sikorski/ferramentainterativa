8e6fa11c2a45cf26522713b130e8346672a70ffc
lui-sikorski <luiz.sikorski@escola.pr.gov.br>
2024-10-23 08:18:29 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -15,8 +15,3 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
-) 
