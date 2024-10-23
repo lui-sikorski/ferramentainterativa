43d451cd4f0507ed1248abe951ea230fb443ab65
lui-sikorski <luiz.sikorski@escola.pr.gov.br>
2024-10-23 09:00:58 -0300

    ok

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+    }
+
     container.appendChild(cartao)
 
 }
